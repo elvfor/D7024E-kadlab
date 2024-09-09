736b38f8cd891f21db7764b133ef7057e6d402ec
elvfor <elvfor-0@student.ltu.se>
2024-09-09 14:26:54 +0200

    test w user input not working

--- kademlia/network.go
+++ kademlia/network.go
@@ -3,6 +3,7 @@ package kademlia
 import (
 	"fmt"
 	"net"
+	"os"
 )
 
 type Network struct {
@@ -37,8 +38,23 @@ func Listen(ip string, port int) {
 
 // PING
 func (network *Network) SendPingMessage(contact *Contact) {
-	//send ping over udp
+	// Resolve the string address to a UDP address
+	udpAddr, err := net.ResolveUDPAddr("udp", contact.Address)
+	// Dial to the address with UDP
+	conn, err := net.DialUDP("udp", nil, udpAddr)
 
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	// Send a message to the server
+	_, err = conn.Write([]byte("Hello UDP Server\n"))
+	fmt.Println("send...")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
 
 func (network *Network) SendFindContactMessage(contact *Contact) {
--- main.go
+++ main.go
@@ -3,21 +3,58 @@
 package main
 
 import (
+	"bufio"
 	"d7024e/kademlia"
 	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
 	fmt.Println("Pretending to run the kademlia app...")
 	// Using stuff from the kademlia package here. Something like...
-	id := kademlia.NewKademliaID("FFFFFFFF00000000000000000000000000000000")
+	id := kademlia.NewRandomKademliaID()
 	contact := kademlia.NewContact(id, "localhost:8000")
 	fmt.Println(contact.String())
 	fmt.Printf("%v\n", contact)
-	routingTable := kademlia.NewRoutingTable(contact)
-	fmt.Println(routingTable)
+	//routingTable := kademlia.NewRoutingTable(contact)
+	//fmt.Println(routingTable)
 
 	//start go routine to call for kademlia.listen
+	// Start the kademlia.Listen function as a goroutine
 	go kademlia.Listen("localhost", 8000)
 
+	go userInputHandler()
+	// Keep the main function running to prevent container exit
+	select {}
+}
+
+// Function to handle user input
+func userInputHandler() {
+	// Create a new reader to read from standard input (os.Stdin)
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		// Print a prompt message
+		fmt.Print("Enter command: ")
+
+		// Read input from standard input until a newline is encountered
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Error reading input:", err)
+			continue
+		}
+
+		// Trim any leading or trailing whitespace from the input
+		input = strings.TrimSpace(input)
+
+		// Check if the input command is "exit"
+		if input == "exit" {
+			fmt.Println("Exiting...")
+			os.Exit(0) // Exit the program
+		}
+
+		// Print the input command
+		fmt.Println("You entered:", input)
+	}
 }
