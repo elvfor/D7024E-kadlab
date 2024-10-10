ae2087eab555c1e7a42f78d283e10a0a74de7236
jennysundstroem <ejyuso-2@student.ltu.se>
2024-10-10 09:03:21 +0200

    tests for bucket added

---
+++ kademlia/bucket_test.go
@@ -0,0 +1,83 @@
+package kademlia
+
+import (
+	"fmt"
+	"testing"
+)
+
+const bucketTestSize = 5
+
+func TestNewBucket(t *testing.T) {
+	bucket := newBucket()
+	if bucket == nil {
+		t.Error("Expected new bucket to be created")
+	}
+	if bucket.list == nil {
+		t.Error("Expected bucket to contain a list")
+	} else {
+		fmt.Println("Test passed. Could create bucket.")
+	}
+}
+
+func TestAddContact(t *testing.T) {
+	bucket := newBucket()
+	contact := NewContact(NewRandomKademliaID(), "127.0.0.1:8000")
+
+	bucketIsFull, lastContact := bucket.AddContact(contact)
+	if bucketIsFull {
+		t.Error("Expected bucket to not be full")
+	}
+	if lastContact != nil {
+		t.Error("Expected lastContact to be nil")
+	}
+
+	// Fill the bucket
+	for i := 0; i < bucketTestSize; i++ {
+		bucket.AddContact(NewContact(NewRandomKademliaID(), "127.0.0.1:8000"))
+	}
+
+	// Add one more contact to fill the bucket
+	bucketIsFull, lastContact = bucket.AddContact(NewContact(NewRandomKademliaID(), "127.0.0.1:8000"))
+	if !bucketIsFull {
+		t.Error("Expected bucket to be full")
+	}
+	if lastContact == nil {
+		t.Error("Expected lastContact to not be nil")
+	}
+	fmt.Println("Test passed. Could add contact to bucket and prevents adding to full bucket.")
+}
+
+func TestRemoveContact(t *testing.T) {
+	bucket := newBucket()
+	contact := NewContact(NewRandomKademliaID(), "127.0.0.1:8000")
+	bucket.AddContact(contact)
+
+	bucket.RemoveContact(&contact)
+	if bucket.list.Len() != 0 {
+		t.Error("Expected contact to be removed from bucket")
+	}
+}
+
+func TestGetContactAndCalcDistance(t *testing.T) {
+	bucket := newBucket()
+	target := NewRandomKademliaID()
+	contact := NewContact(NewRandomKademliaID(), "127.0.0.1:8000")
+	bucket.AddContact(contact)
+
+	contacts := bucket.GetContactAndCalcDistance(target)
+	if len(contacts) != 1 {
+		t.Error("Expected one contact in the bucket")
+	}
+}
+
+func TestLen(t *testing.T) {
+	bucket := newBucket()
+	if bucket.Len() != 0 {
+		t.Error("Expected bucket length to be 0")
+	}
+	contact := NewContact(NewRandomKademliaID(), "127.0.0.1:8000")
+	bucket.AddContact(contact)
+	if bucket.Len() != 1 {
+		t.Error("Expected bucket length to be 1")
+	}
+}
