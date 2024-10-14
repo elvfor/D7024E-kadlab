a8e8be429f43a303bd0a4d9df5f9f5a43f3abff6
jennysundstroem <ejyuso-2@student.ltu.se>
2024-10-14 11:00:40 +0200

    more cli tests

--- cli/cli.go
+++ cli/cli.go
@@ -39,54 +39,66 @@ func UserInputHandler(k *kademlia.Kademlia, reader io.Reader, writer io.Writer) 
 			continue
 		}
 
-		fmt.Fprintf(writer, "You entered: command=%s, argument=%s\n", command, arg)
-
-		switch command {
-		case "PING":
-			handlePing(k, arg)
-		case "GET":
-			handleGet(k, arg)
-		case "PUT":
-			handlePut(k, arg)
-		case "EXIT":
-			fmt.Fprintln(writer, "Exiting program.")
+		if handleCommand(k, command, arg, writer) {
 			return
-		case "LOOKUP":
-			handleLookup(k, arg)
-		case "PRINT":
-			k.ActionChannel <- kademlia.Action{Action: "PRINT"}
-		default:
-			fmt.Fprintln(writer, "Error: Unknown command.")
 		}
 	}
 }
 
-func handlePing(k *kademlia.Kademlia, arg string) {
-	if arg != "" {
-		contact := kademlia.NewContact(kademlia.NewRandomKademliaID(), strings.TrimSpace(arg))
-		if k.Network.SendPingMessage(&k.RoutingTable.Me, &contact) {
-			k.UpdateRT(contact.ID, contact.Address)
-		}
-	} else {
-		fmt.Println("Error: No argument provided for PING.")
+func handleCommand(k *kademlia.Kademlia, command, arg string, writer io.Writer) bool {
+	fmt.Fprintf(writer, "You entered: command=%s, argument=%s\n", command, arg)
+
+	switch command {
+	case "GET":
+		handleGet(k, arg)
+	case "PUT":
+		handlePut(k, arg)
+	case "EXIT":
+		fmt.Fprintln(writer, "Exiting program.")
+		return true
+	case "PRINT":
+		k.ActionChannel <- kademlia.Action{Action: "PRINT"}
+	default:
+		fmt.Fprintln(writer, "Error: Unknown command.")
 	}
+	return false
 }
 
 func handleGet(k *kademlia.Kademlia, arg string) {
-	if arg == "" {
-		fmt.Println("Error: No argument provided for GET.")
+	if err := ValidateGetArg(arg); err != nil {
+		fmt.Println(err)
 		return
+	}
+
+	targetContact := CreateTargetContact(arg)
+	foundOnContact, foundData := performNodeLookup(k, targetContact, arg)
+	HandleLookupResult(foundOnContact, foundData)
+}
+
+func ValidateGetArg(arg string) error {
+	if arg == "" {
+		return fmt.Errorf("error: No argument provided for GET")
 	}
 
 	if len(arg) != 40 { // Kademlia ID length
-		fmt.Println("Error: Invalid Kademlia ID length.")
-		return
+		return fmt.Errorf("error: Invalid Kademlia ID length")
 	}
 
-	targetContact := kademlia.NewContact(kademlia.NewKademliaID(arg), "")
-	_, foundOncontact, foundData := k.NodeLookup(&targetContact, arg)
+	return nil
+}
+
+func CreateTargetContact(arg string) kademlia.Contact {
+	return kademlia.NewContact(kademlia.NewKademliaID(arg), "")
+}
+
+func performNodeLookup(k *kademlia.Kademlia, targetContact kademlia.Contact, arg string) (kademlia.Contact, []byte) {
+	_, foundOnContact, foundData := k.NodeLookup(&targetContact, arg)
+	return foundOnContact, foundData
+}
+
+func HandleLookupResult(foundOnContact kademlia.Contact, foundData []byte) {
 	if foundData != nil {
-		fmt.Println("Data found on contact:", foundOncontact.String())
+		fmt.Println("Data found on contact:", foundOnContact.String())
 		fmt.Println("Data:", string(foundData))
 	} else {
 		fmt.Println("Data not found.")
@@ -94,54 +106,72 @@ func handleGet(k *kademlia.Kademlia, arg string) {
 }
 
 func handlePut(k *kademlia.Kademlia, arg string) {
-	if arg != "" {
-		data := []byte(arg)
-		hasher := sha1.New()
-		hasher.Write([]byte("hash1"))
-		hash := hasher.Sum(nil)
-		hashString := hex.EncodeToString(hash)
-		kadId := kademlia.NewKademliaID(hashString)
-		targetContact := kademlia.NewContact(kadId, "")
-		contacts, _, _ := k.NodeLookup(&targetContact, "")
-		resultChan := make(chan bool, len(contacts))
-		var wg sync.WaitGroup
-
-		for _, contact := range contacts {
-			wg.Add(1)
-			go func(contact kademlia.Contact) {
-				defer wg.Done()
-				result := k.Network.SendStoreMessage(&k.RoutingTable.Me, &contact, kadId, data)
-				fmt.Println("Storing data with key:", kadId.String(), "on contact:", contact.String())
-				resultChan <- result
-			}(contact)
-		}
-
-		go func() {
-			wg.Wait()
-			close(resultChan)
-		}()
-
-		successCount := 0
-		for success := range resultChan {
-			if success {
-				successCount++
-			}
-		}
-		if successCount > len(contacts)/2 {
-			fmt.Println("Data stored successfully. Hash:" + kadId.String())
-		} else {
-			fmt.Println("Failed to store data.")
-		}
-	} else {
-		fmt.Println("Error: No argument provided for PUT.")
+	if err := ValidatePutArg(arg); err != nil {
+		fmt.Println(err)
+		return
 	}
+
+	data := []byte(arg)
+	kadId, targetContact := CreatePutTargetContact(data)
+	contacts := performPutNodeLookup(k, targetContact)
+	successCount := storeDataOnContacts(k, kadId, data, contacts)
+	HandleStoreResult(successCount, len(contacts))
 }
 
-func handleLookup(k *kademlia.Kademlia, arg string) {
-	if arg != "" {
-		contact := kademlia.NewContact(kademlia.NewRandomKademliaID(), strings.TrimSpace(arg))
-		bootStrapContact := kademlia.NewContact(kademlia.NewKademliaID("FFFFFFFFF0000000000000000000000000000000)"), "172.20.0.6")
-		contacts, _ := k.Network.SendFindContactMessage(&contact, &bootStrapContact, &contact)
-		fmt.Print(contacts)
+func ValidatePutArg(arg string) error {
+	if arg == "" {
+		return fmt.Errorf("Error: No argument provided for PUT.")
+	}
+	return nil
+}
+
+func CreatePutTargetContact(data []byte) (*kademlia.KademliaID, kademlia.Contact) {
+	hasher := sha1.New()
+	hasher.Write([]byte("hash1"))
+	hash := hasher.Sum(nil)
+	hashString := hex.EncodeToString(hash)
+	kadId := kademlia.NewKademliaID(hashString)
+	targetContact := kademlia.NewContact(kadId, "")
+	return kadId, targetContact
+}
+
+func performPutNodeLookup(k *kademlia.Kademlia, targetContact kademlia.Contact) []kademlia.Contact {
+	contacts, _, _ := k.NodeLookup(&targetContact, "")
+	return contacts
+}
+
+func storeDataOnContacts(k *kademlia.Kademlia, kadId *kademlia.KademliaID, data []byte, contacts []kademlia.Contact) int {
+	resultChan := make(chan bool, len(contacts))
+	var wg sync.WaitGroup
+
+	for _, contact := range contacts {
+		wg.Add(1)
+		go func(contact kademlia.Contact) {
+			defer wg.Done()
+			result := k.Network.SendStoreMessage(&k.RoutingTable.Me, &contact, kadId, data)
+			fmt.Println("Storing data with key:", kadId.String(), "on contact:", contact.String())
+			resultChan <- result
+		}(contact)
+	}
+
+	go func() {
+		wg.Wait()
+		close(resultChan)
+	}()
+
+	successCount := 0
+	for success := range resultChan {
+		if success {
+			successCount++
+		}
+	}
+	return successCount
+}
+
+func HandleStoreResult(successCount, totalContacts int) {
+	if successCount > totalContacts/2 {
+		fmt.Println("Data stored successfully.")
+	} else {
+		fmt.Println("Failed to store data.")
 	}
 }
